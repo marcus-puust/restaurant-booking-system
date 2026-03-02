49683589096a062a125d6f0ce0f623bb8b93b172
Marcus Puust <marcusp@tlu.ee>
2026-03-02 19:45:44 +0200

    feat: add table filtering methods for availability (capacity, active, zone)

--- backend/src/main/java/com/marcus/restaurant_booking/repository/RestaurantTableRepository.java
+++ backend/src/main/java/com/marcus/restaurant_booking/repository/RestaurantTableRepository.java
@@ -3,5 +3,11 @@ package com.marcus.restaurant_booking.repository;
 import com.marcus.restaurant_booking.model.RestaurantTable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RestaurantTableRepository extends JpaRepository<RestaurantTable, Long> {
+
+    List<RestaurantTable> findByActiveTrueAndCapacityGreaterThanEqual(Integer partySize);
+
+    List<RestaurantTable> findByActiveTrueAndCapacityGreaterThanEqualAndZone_Id(Integer partySize, Long zoneId);
 }
